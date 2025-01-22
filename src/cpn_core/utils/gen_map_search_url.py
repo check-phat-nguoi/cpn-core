0ed75c75109cc768c67884c51d3e1813a65aa92d
NTGNguyen <23521049@gm.uit.edu.vn>
2025-01-22 10:03:29 +0700

    chore: use concat string in stead of format string
    - Change to use concat string in Google map query string becau
    se of it's faster

--- src/cpn_core/utils/gen_map_search_url.py
+++ src/cpn_core/utils/gen_map_search_url.py
@@ -1,8 +1,8 @@
 from typing import LiteralString
 from urllib.parse import quote_plus
 
-GOOGLE_MAPS_QUERY_STRING: LiteralString = "https://www.google.com/maps/search/{query}"
+GOOGLE_MAPS_QUERY_STRING: LiteralString = "https://www.google.com/maps/search/"
 
 
 def gen_map_search_url(location: str) -> str:
-    return GOOGLE_MAPS_QUERY_STRING.format(query=quote_plus(location))
+    return GOOGLE_MAPS_QUERY_STRING + quote_plus(location)
